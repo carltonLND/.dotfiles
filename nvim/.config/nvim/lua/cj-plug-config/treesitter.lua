2bc6cc969f876cb829682b34093ca085e9888138
Carlton (CJ) <carltonlnd@hotmail.com>
2021-12-07 10:38:31 +0000

    added ftplugin

--- nvim/.config/nvim/lua/cj-plug-config/treesitter.lua
+++ nvim/.config/nvim/lua/cj-plug-config/treesitter.lua
@@ -1,9 +1,9 @@
 local treesitter = require("nvim-treesitter.configs")
 
 treesitter.setup {
-    ensure_installed = "maintained",
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-    },
+  ensure_installed = "maintained",
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
 }
