2733b676ca26a51c29f471cbec83c48067ff3d9e
Carlton (CJ) <carltonlnd@hotmail.com>
2021-12-15 11:57:36 +0000

    minor changes for macos

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,6 +8,7 @@ eval (/opt/homebrew/bin/brew shellenv)
 # exports
 set -x EDITOR nvim
 set -x DOT ~/.dotfiles
+set -x PROJECTS ~/Projects
 set -x FISHDIR ~/.config/fish/
 set -x VIMDIR ~/.config/nvim/
 set -x KITTYDIR ~/.config/kitty/
