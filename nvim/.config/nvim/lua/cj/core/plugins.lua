5a04a1341e67165567b7cdeef630e733696ec04b
Carlton (CJ) <carltonlnd@hotmail.com>
2022-06-16 19:27:47 +0100

    Added telescope-ui-select

--- nvim/.config/nvim/lua/cj/core/plugins.lua
+++ nvim/.config/nvim/lua/cj/core/plugins.lua
@@ -33,6 +33,7 @@ return packer.startup(function(use)
     "nvim-telescope/telescope.nvim",
     requires = {
       "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-ui-select.nvim",
       {
         "nvim-telescope/telescope-fzf-native.nvim",
         run = "make",
--- nvim/.config/nvim/lua/cj/plugins/telescope/init.lua
+++ nvim/.config/nvim/lua/cj/plugins/telescope/init.lua
@@ -1,6 +1,13 @@
 -- Setup telescope
 local telescope = require("telescope")
-telescope.setup({})
+telescope.setup({
+  extensions = {
+    ["ui-select"] = {
+      require("telescope.themes").get_dropdown({}),
+    },
+  },
+})
 
 -- Load extensions
 telescope.load_extension("fzf")
+telescope.load_extension("ui-select")
