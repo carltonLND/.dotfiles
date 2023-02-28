1058c547a7136147f9092ab18fed63c60e450b3c
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-02-28 13:29:38 +0000

    Added autosaving for rust files

--- config/nvim/lua/cj/plugins/core.lua
+++ config/nvim/lua/cj/plugins/core.lua
@@ -1,4 +1,25 @@
 return {
-	"lewis6991/impatient.nvim",
-	priority = 999,
+	{ "lewis6991/impatient.nvim", priority = 999 },
+	{
+		"Pocco81/auto-save.nvim",
+		config = true,
+		opts = {
+			execution_message = { message = "" },
+			trigger_events = { "InsertLeave", "TextChanged" },
+			condition = function(buf)
+        -- Only autosave rust files for rust-analyzer
+        if vim.bo.filetype ~= "rust" then
+          return false
+        end
+
+				local fn = vim.fn
+				local utils = require("auto-save.utils.data")
+
+				if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+					return true
+				end
+				return false
+			end,
+		},
+	},
 }
