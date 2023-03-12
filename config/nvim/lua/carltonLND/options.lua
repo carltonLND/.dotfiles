0197086f35b0f128c8412d702aaa5cfcd6eca493
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-03-12 17:42:02 +0000

    Added netrw settings

--- config/nvim/lua/carltonLND/options.lua
+++ config/nvim/lua/carltonLND/options.lua
@@ -3,6 +3,8 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 vim.g.markdown_recommended_style = 0
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
 
 -- :set
 vim.opt.autowrite = true -- Enable auto write
@@ -29,7 +31,6 @@ vim.opt.showmode = false -- Dont show mode since we have a statusline
 vim.opt.sidescrolloff = 8 -- Columns of context
 vim.opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
 vim.opt.smartcase = true -- Don't ignore case with capitals
--- vim.opt.smartindent = true -- Insert indents automatically
 vim.opt.spelllang = { "en" }
 vim.opt.splitbelow = true -- Put new windows below current
 vim.opt.splitright = true -- Put new windows right of current
