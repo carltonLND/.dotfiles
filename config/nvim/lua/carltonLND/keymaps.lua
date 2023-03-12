1d79aaea6b270d9a8db15dc41ce0124fd6a06643
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-03-12 17:58:39 +0000

    Added QOL keymaps and tmux-sessionizer map

--- config/nvim/lua/carltonLND/keymaps.lua
+++ config/nvim/lua/carltonLND/keymaps.lua
@@ -5,6 +5,10 @@ vim.keymap.set("n", "x", '"_x', opts)
 vim.keymap.set("n", "x", '"_x', opts)
 vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
 vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts)
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
+vim.keymap.set("n", "n", "nzzzv", opts)
+vim.keymap.set("n", "N", "Nzzzv", opts)
 vim.keymap.set("v", "<", "<gv", opts)
 vim.keymap.set("v", ">", ">gv", opts)
 vim.keymap.set("v", "<leader>y", '"+y', opts)
@@ -13,6 +17,15 @@ vim.keymap.set("x", "p", "P", opts)
 vim.keymap.set("x", "P", "p", opts)
 
 -- Removed keymaps
-vim.keymap.set("n", "s", "", opts)
-vim.keymap.set("n", "q", "", opts)
-vim.keymap.set("v", "K", "", opts)
+vim.keymap.set("n", "s", "<nop>", opts)
+vim.keymap.set("n", "q", "<nop>", opts)
+vim.keymap.set("n", "Q", "<nop>", opts)
+vim.keymap.set("v", "K", "<nop>", opts)
+
+-- External keymaps
+vim.keymap.set(
+  "n",
+  "<C-f>",
+  "<cmd>silent !tmux neww tmux-sessionizer<CR>",
+  opts
+)
