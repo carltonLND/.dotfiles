66ab57e37d146e4df445f9327d4931e7f5a77f56
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-03-14 01:51:32 +0000

    Added more quality of life keymaps

--- config/nvim/lua/carltonLND/keymaps.lua
+++ config/nvim/lua/carltonLND/keymaps.lua
@@ -5,22 +5,37 @@ vim.keymap.set("n", "x", '"_x', opts)
 vim.keymap.set("n", "x", '"_x', opts)
 vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
 vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts)
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv", opts)
+vim.keymap.set("v", "K", ":m '<-2<CR>gv", opts)
 vim.keymap.set("n", "n", "nzzzv", opts)
 vim.keymap.set("n", "N", "Nzzzv", opts)
 vim.keymap.set("v", "<", "<gv", opts)
 vim.keymap.set("v", ">", ">gv", opts)
 vim.keymap.set("v", "<leader>y", '"+y', opts)
+vim.keymap.set("n", "<leader>y", '"+y', opts)
 vim.keymap.set("n", "<leader>p", '"+p', opts)
 vim.keymap.set("x", "p", "P", opts)
 vim.keymap.set("x", "P", "p", opts)
+vim.keymap.set("n", "J", "mzJ`z", opts)
+
+-- Deal with text wrapping cases
+vim.keymap.set(
+  "n",
+  "k",
+  "v:count == 0 ? 'gk' : 'k'",
+  { expr = true, silent = true, noremap = true }
+)
+vim.keymap.set(
+  "n",
+  "j",
+  "v:count == 0 ? 'gj' : 'j'",
+  { expr = true, silent = true, noremap = true }
+)
 
 -- Removed keymaps
 vim.keymap.set("n", "s", "<nop>", opts)
 vim.keymap.set("n", "q", "<nop>", opts)
 vim.keymap.set("n", "Q", "<nop>", opts)
-vim.keymap.set("v", "K", "<nop>", opts)
 
 -- External keymaps
 vim.keymap.set(
