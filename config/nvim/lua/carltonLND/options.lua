51bfbea135db5d8df8233ce346e6548e1eac0668
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-03-17 05:51:25 +0000

    Don't load providers as we're not using remote plugins

--- config/nvim/lua/carltonLND/options.lua
+++ config/nvim/lua/carltonLND/options.lua
@@ -2,9 +2,14 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.g.markdown_recommended_style = 0
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
+
+-- Disable providers as we're not using remote plugins
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_node_provider = 0
 
 -- :set
 vim.opt.autowrite = true -- Enable auto write
