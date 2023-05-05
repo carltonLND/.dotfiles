3073ed3b2665cc68c8122bb31615966a2e80300d
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-05-05 21:43:33 +0100

    Fixed formatting on save stopping when opening new language buffer

--- config/nvim/lua/carltonLND/plugins/lsp.lua
+++ config/nvim/lua/carltonLND/plugins/lsp.lua
@@ -300,8 +300,7 @@ return {
           null_ls.builtins.formatting.gofumpt,
         },
         on_attach = function(client, bufnr)
-          local augroup =
-            vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+          local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
           if client.supports_method "textDocument/formatting" then
             vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
