d7d207c468afa9178954d1cc2fad2fea6262c0cf
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-06-30 13:53:47 +0100

    Add incremental selection support

--- config/nvim/lua/carltonLND/plugins/core.lua
+++ config/nvim/lua/carltonLND/plugins/core.lua
@@ -29,6 +29,15 @@ return {
         indent = { enable = true },
         autotag = { enable = true },
         additional_vim_regex_highlighting = false,
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<CR>",
+            scope_incremental = "<CR>",
+            node_incremental = "<TAB>",
+            node_decremental = "<S-TAB>",
+          },
+        },
       }
     end,
   },
