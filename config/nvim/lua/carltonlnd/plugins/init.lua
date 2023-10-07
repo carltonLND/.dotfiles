93ea2de7c5a8a7485b4d58b891caa69f7407ebf4
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-10-07 12:57:04 +0100

    Add github copilot

--- config/nvim/lua/carltonlnd/plugins/init.lua
+++ config/nvim/lua/carltonlnd/plugins/init.lua
@@ -2,6 +2,28 @@ return {
   "christoomey/vim-tmux-navigator",
   "inkarkat/vim-ReplaceWithRegister",
   {
+    "github/copilot.vim",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      vim.g.copilot_enabled = false
+      vim.g.copilot_no_tab_map = true
+
+      vim.api.nvim_set_keymap(
+        "i",
+        "<A-Enter>",
+        'copilot#Accept("<CR>")',
+        { silent = true, expr = true }
+      )
+      vim.api.nvim_set_keymap(
+        "n",
+        "<leader>cp",
+        "<CMD>Copilot<CR>",
+        { silent = true }
+      )
+    end,
+  },
+  {
     "iamcco/markdown-preview.nvim",
     build = function()
       vim.fn["mkdp#util#install"]()
